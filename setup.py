dc429a9de8c6c44f1e228c57eb398e3d426a7a93
Mason Proffitt <mproffit@cern.ch>
2020-01-25 22:40:47 +0100

    split setup.py line for flake8

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(name='func_adl_uproot',
                  version=0.1,
-                 description='Functional Analysis Description Language uproot backend for accessing flat ROOT ntuples',
+                 description=('Functional Analysis Description Language'
+                              + ' uproot backend for accessing flat ROOT ntuples'),
                  long_description=long_description,
                  long_description_content_type="text/markdown",
                  packages=setuptools.find_packages(exclude=['tests']),
