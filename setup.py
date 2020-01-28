06b5f9965f17413aec64287bc67c18e81faa65c0
Mason Proffitt <mproffit@cern.ch>
2020-01-28 10:22:00 +0100

    update to v0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='func_adl_uproot',
-                 version=0.4,
+                 version=0.5,
                  description=('Functional Analysis Description Language'
                               + ' uproot backend for accessing flat ROOT ntuples'),
                  long_description=long_description,
