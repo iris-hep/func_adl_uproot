c33283f25b592ff868ac812bf6986b7b545a7a66
Mason Proffitt <mproffit@cern.ch>
2020-11-11 15:26:01 +0100

    add Select test with dict

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -12,6 +12,13 @@ def test_ast_executor_select_scalar_branch():
     assert ast_executor(python_ast).tolist() == [0, -1]
 
 
+def test_ast_executor_select_scalar_branch_dict():
+    python_source = ("Select(EventDataset('tests/scalars_tree_file.root', 'tree'),"
+                     + " lambda row: {'int_branch': row.int_branch})")
+    python_ast = qastle.insert_linq_nodes(ast.parse(python_source))
+    assert ast_executor(python_ast)['int_branch'l].tolist() == [0, -1]
+
+
 def test_ast_executor_where_scalar_branch():
     python_source = ("Where(EventDataset('tests/scalars_tree_file.root', 'tree'),"
                      + ' lambda row: row.int_branch < 0)'
@@ -34,7 +41,7 @@ def test_ast_executor_selectmany_vector_branch():
     assert ast_executor(python_ast).tolist() == [-1, 2, 3, 13]
 
 
-def test_ast_executor_selectmany_vector_branch_dictionary():
+def test_ast_executor_selectmany_vector_branch_dict():
     python_source = ("SelectMany(EventDataset('tests/vectors_tree_file.root', 'tree'),"
                      + " lambda row: {'int_vector_branch': row.int_vector_branch})")
     python_ast = qastle.insert_linq_nodes(ast.parse(python_source))
