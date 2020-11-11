646810151fd0595e797c29ccce3a815bfa3ad22f
Mason Proffitt <mason.proffitt@cern.ch>
2020-11-11 12:26:13 +0100

    update qastle requirement

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(name='func_adl_uproot',
                  python_requires=('>=2.7, '
                                   '!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <3.10'),
                  install_requires=['awkward>=0.12.17',
-                                   'qastle>=0.7',
+                                   'qastle>=0.9',
                                    'uproot>=3.6.0'],
                  author='Mason Proffitt',
                  author_email='masonlp@uw.edu',
