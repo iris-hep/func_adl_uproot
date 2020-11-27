3d15c9b079a6bf2aad2adb7252c9abd189193731
Mason Proffitt <mason.proffitt@cern.ch>
2020-11-27 17:33:10 +0100

    remove unnecessary qastle calls

--- func_adl_uproot/executor.py
+++ func_adl_uproot/executor.py
@@ -1,9 +1,6 @@
-import qastle
-
 from .translation import generate_function
 
 
 def ast_executor(ast):
-    qastle.insert_linq_nodes(ast)
     query_function = generate_function(ast)
     return query_function()
