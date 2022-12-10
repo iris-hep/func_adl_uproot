174306406269f7a64df068483c76c566a7c4857a
Mason Proffitt <mason.proffitt@cern.ch>
2022-12-10 01:46:49 +0100

    move to v1.10.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='func_adl_uproot',
-    version='1.10.0',
+    version='1.10.1',
     description=(
         'Functional Analysis Description Language'
         + ' uproot backend for accessing flat ROOT ntuples'
