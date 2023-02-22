aed2e68c6643bfc955ba45742874fe303201a467
Mason Proffitt <mason.proffitt@cern.ch>
2023-02-22 18:31:23 +0100

    use semantic versioning compatible RC version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='func_adl_uproot',
-    version='2.0.0rc1',
+    version='2.0.0-rc.1',
     description=(
         'Functional Analysis Description Language'
         + ' uproot backend for accessing flat ROOT ntuples'
